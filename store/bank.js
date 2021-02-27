e1c11ce2007294aae6a96b449201f7fbe10d7fe2
Angelo <angelo@rcstart.it>
2021-02-27 06:38:19 +0100

    created store/bank

---
+++ store/bank.js
@@ -0,0 +1,71 @@
+import Client from "@/lib/btsg-ws";
+
+export const state = () => ({
+  coins: []
+})
+
+export const mutations = {
+  setCoins: (state, payload) => {
+    state.coins = payload
+  },
+}
+
+export const getters = {
+  balance: state => {
+    return state.coins.find(c => c.denom === process.env.MICROSTAKEDENOM) || { denom: process.env.MICROSTAKEDENOM, amount: 0 }
+  },
+  coins: state => {
+    return state.coins
+  },
+}
+
+export const actions = {
+  async updateBalance({
+    commit, rootGetters
+  }) {
+    try {
+      const address = rootGetters['wallet/address']
+
+      if (address !== null) {
+        const response = await this.$btsg.getAccount(address)
+        commit('setCoins', response.value.coins)
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  },
+  subscribe({
+    dispatch, rootGetters
+  }) {
+    try {
+      const address = rootGetters['wallet/address']
+
+      const client = new Client(
+        "bitsong-testnet-4",
+        "wss://rpc.testnet4.bitsong.network/websocket"
+      );
+
+      client.subscribe(
+        {
+          query:
+            `transfer.recipient='${address}'`
+        },
+        response => {
+          dispatch('updateBalance')
+        }
+      );
+
+      client.subscribe(
+        {
+          query:
+            `transfer.sender='${address}'`
+        },
+        response => {
+          dispatch('updateBalance')
+        }
+      );
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
